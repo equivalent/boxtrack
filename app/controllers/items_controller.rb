62a076280f8945b173f47cc61835b6f422cf9986
Tomas Valent <equivalent@eq8.eu>
2023-10-18 12:52:06 +0200

    4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -65,6 +65,6 @@ class ItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def item_params
-      params.require(:item).permit(:title, :box_id)
+      params.require(:item).permit(:title, :box_id, :image)
     end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :box
+  has_one_attached :image
 
   validates_presence_of :title
 end
